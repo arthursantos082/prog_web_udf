e055643172383a488e85f8c6497e1ce678fe49d0
Arthur <arthursantos0017@gmail.com>
2025-09-17 00:09:51 -0300

    listagem arrumada

--- aula004/cadastro.php
+++ aula004/cadastro.php
@@ -1,38 +1,101 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cadastro</title>
-</head>
-<body>
-    
-    <div class="container" style="text-align: center;">
-        <h2>Cadastro de Pessoas</h2>
-        <!--Mensagens-->
-        
-        <div class="mensagem">
-            <?php 
-            if (isset($_GET['msg'])) { ?>
-                <p style='color: <?= $_GET['tipo'] === 'ok' ? 'green': 'red'?>;'>
-            <?php } ?>
-        </div>
+<html lang="pt-BR">
 
+<head>
+    <meta charset="UTF-8" />
+    <title>Cadastro de Pessoa</title>
+    <style>
+        body {
+            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+            background: linear-gradient(135deg, #4E73DF, #1CC88A);
+            margin: 0px;
+            height: 100vh;
+            display: flex;
+            justify-content: center; /* Horizontal */
+            align-items: center; /* Vertical */
+        }
+        .container {
+            background-color: #fff;
+            padding: 2rem;
+            border-radius: 15px;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+            width: 350px;
+            text-align: center;
+            animation: fade-in 0.8s ease-in-out;
+        }
+        h2 {
+            margin-bottom: 1.5rem;
+            color: #333;
+        }
+        label {
+            display: block;
+            margin: 10px 0 5px;
+            text-align: left;
+            font-weight: bold;
+            color: #666;
+        }
+        input, select {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border-radius: 8px;
+            border: 1px solid #ccc;
+            font-size: 14px;
+            transition: border-color 0.3s;
+        }
+        input:focus , select:focus {
+            border-color: #4E73DF;
+            outline: none;
+        }
+        button {
+            background-color: #4E73DF;
+            color: #FFF;
+            font-size: 16px;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        button:Hover {
+            background-color: #32be22ff;
+        }
+        @keyframes fade-in {
+            from { opacity: 0; transform: scale(0.0); }
+            to {opacity: 1; transform: scale(1.0); }            
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Cadastro de Pessoas</h2>
+        <!-- Mensagens -->
+        <div class="mensagem">
+            <?php if (isset($_GET['msg'])) { ?>
+                <p style='color: <?= $_GET['tipo'] === 'ok' ? 'green': 'red'?>;'>
+                    <?= htmlspecialchars($_GET['msg']) ?> 
+                </p>    
+            <?php } ?>
+        </div>    
+        <!-- Pagina -->
         <form action="gravar.php" method="POST">
-            <label for="nome">Nome: </label>
+            <label for="nome">Nome:</label>
             <input type="text" id="nome" name="nome" require />
-            <label for="idade">Idade: </label>
+            <label for="idade">Idade:</label>
             <input type="number" id="idade" name="idade" require />
-            <label for="estadoCivil">Estado Civil: </label>
-            <select name="estadoCivil" id="estadoCivil">
+            <label for="estadoCivil">Estado Civil:</label>
+            <select id="estadoCivil" name="estadoCivil">
                 <option value="">Selecione o Estado Civil</option>
                 <option value="C">Casado(a)</option>
                 <option value="S">Solteiro(a)</option>
-            </select>
+            </select>    
             <button type="submit">Enviar</button>
-            <button onclick="window.location.href='listagem.php'">Ver a Listagem</button>
-        </form>
+        </form>    
     </div>
 
+    <button onclick="window.location.href='listagem.php'">Ver a Listagem</button>
+
 </body>
-</html>
+
+</html>    
--- aula004/listagem.php
+++ aula004/listagem.php
@@ -1,52 +1,103 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-
 <?php
-header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Cache-Control: no-cache, no-store, must-revalidade");
 header("Pragma: no-cache");
+header("Expires: 0");
 
 $arquivo = "dados.txt";
 $linhas = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 ?>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>listagem</title>
+<head>
+    <meta charset="UTF-8" />
+    <title>Cadastro de Pessoa</title>
+    <style>
+        body {
+            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+            background: linear-gradient(135deg, #4E73DF, #1CC88A);
+            margin: 0px;
+            height: 100vh;
+            display: flex;
+            justify-content: center; /* Horizontal */
+            align-items: center; /* Vertical */
+        }
+        .container {
+            background-color: #fff;
+            padding: 2rem;
+            border-radius: 15px;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+            width: 350px;
+            text-align: center;
+            animation: fade-in 0.8s ease-in-out;
+        }
+        h2 {
+            margin-bottom: 1.5rem;
+            color: #333;
+        }
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #333;
+            padding: 8px 12px;
+            text-align: center
+        }
+        th {
+            background-color: #F2F2F2;
+        }
+        button {
+            background-color: #4E73DF;
+            color: #FFF;
+            font-size: 16px;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        button:Hover {
+            background-color: #32be22ff;
+        }
+        @keyframes fade-in {
+            from { opacity: 0; transform: scale(0.0); }
+            to {opacity: 1; transform: scale(1.0); }            
+        }
+    </style>
 </head>
+
+
 <body>
-    
     <div class="container">
         <h2>Listagem de Pessoas</h2>
         <table>
             <thead>
                 <tr>
                     <?php
-                    $cabecalho = explode(";",$linhas[0]);
-                    foreach($cabecalho as $coluna) {
-                        echo "<th>".htmlspecialchars($coluna)."</th>";
-                    }
-                    ?>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php
-                        for($i = 1; $i < count($linhas); $i++) {
-                            $colunas = explode(";",$linhas[0]);
-                            echo "<tr>";
-                            foreach($colunas as $coluna) {
-                                echo "<td>".htmlspecialchars($coluna)."</th>";
-                            }
-                            echo "</tr>";
+                        $cabecalho = explode(";", $linhas[0]);
+                        foreach ($cabecalho as $coluna) {
+                            echo "<th>".htmlspecialchars($coluna)."</th>";
                         }
                     ?>
-                </tr>
-            </tbody>
-        </table>
-        
+                </tr>    
+            </thead>
+            <tbody>
+                <?php
+                    for ($i = 1; $i < count($linhas); $i++) {
+                        $colunas = explode(";", $linhas[$i]);
+                        echo "<tr>";
+                        foreach ($colunas as $coluna) {
+                            echo "<td>".htmlspecialchars($coluna)."</td>";
+                        }
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>        
+        </table>    
+
         <a href="cadastro.php">Cadastrar Dados</a>
     </div>
-
 </body>
+
 </html>
